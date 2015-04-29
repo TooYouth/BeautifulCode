598da99687037967b8a4f91fa1d4b2cdde41dcd8
TooYouth <luffycuidc@163.com>
2015-04-29 20:16:42 +0800

    refresh

---
+++ BeautifulCode/src/First/First.java
@@ -0,0 +1,18 @@
+package First;
+/**让CPU听你的指挥
+ * 由于java的系统调用不及C++，暂时放弃
+ * */
+public class First {
+	
+	public static void main(String[] args) throws InterruptedException {
+		line();
+	}
+	public static void line() throws InterruptedException{
+		while(true){
+			for(int i=0;i<96000000;i++){
+				
+			}
+			Thread.sleep(100);
+		}
+	}
+}
